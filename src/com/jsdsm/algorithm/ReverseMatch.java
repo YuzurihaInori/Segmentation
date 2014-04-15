7faf1824288131889a1e9dac65c3c404eccd98b0
YuzurihaInori <823130614@qq.com>
2014-04-15 14:22:32 +0800

    add reversematch

--- src/com/jsdsm/algorithm/ReverseMatch.java
+++ src/com/jsdsm/algorithm/ReverseMatch.java
@@ -1,5 +1,9 @@
 package com.jsdsm.algorithm;
 
+import java.util.LinkedList;
+
+import com.jsdsm.dictionary.SearchDictionary;
+
 /**
  *逆向匹配算法类 {@link Algorithm}}
  * @author jisi
@@ -7,4 +11,46 @@ package com.jsdsm.algorithm;
  */
 public class ReverseMatch extends Algorithm {
 
+	public static LinkedList<String> tempList;
+
+	public LinkedList<String> startReverseMatch(String input) {
+		tempList = new LinkedList<String>();
+		new Split(input).start();
+		return tempList;
+	}
+
+	class Split {
+		private String input = null;
+
+		public Split(String input) {
+			this.input = input;
+		}
+
+		public void start() {
+			String temp = null;
+			for (int i = 0; i < this.input.length(); i++) {
+				temp = this.input.substring(i); // 每次从字符串的首部截取一个字，并存到temp中
+				// System.out.println("*****" + temp + "*********" +
+				// this.input);
+				// 如果该词在字典中， 则删除该词并在原始字符串中截取该词
+				if (SearchDictionary.searchDictionary(temp) > 0) {
+					System.out.println("suc=" + temp);
+					this.input = this.input.replace(temp, "");
+					i = -1; // i=-1是因为要重新查找， 而要先执行循环中的i++
+					ReverseMatch.tempList.addFirst("/" + temp);
+				} else {
+					System.out.println("fal=" + temp);
+				}
+				if ((input.length() - i) == 1) {
+					ReverseMatch.tempList.addFirst("/" + temp);
+				}
+			}
+
+			// 当前循环完毕，词的末尾截去一个字，继续循环， 直到词变为空
+			if (null != this.input && !"".equals(this.input)) {
+				this.input = this.input.substring(0, this.input.length() - 1);
+				this.start();
+			}
+		}
+	}
 }
---
+++ src/com/jsdsm/dictionary/test.java
@@ -0,0 +1,29 @@
+package com.jsdsm.dictionary;
+
+import java.io.IOException;
+import java.util.LinkedList;
+
+import org.apache.lucene.index.CorruptIndexException;
+import org.apache.lucene.queryParser.ParseException;
+import org.apache.lucene.store.LockObtainFailedException;
+
+import com.jsdsm.algorithm.ReverseMatch;
+
+public class test {
+
+	public static void main(String[] args) throws CorruptIndexException, LockObtainFailedException, IOException, ParseException {
+		System.out.println("-----------建立索引----------------");
+		boolean status = IndexDictionary.createIndex();
+		if (status) {
+			System.out.println("-----------索引结束----------------");
+		}else {
+			System.out.println("-----------索引失败----------------");
+			return;
+		}
+		String word = "我们聊聊吧";
+		LinkedList<String> data = new ReverseMatch().startReverseMatch(word);
+		System.out.println(data.toString());
+//		int count = SearchDictionary.searchDictionary(word);
+//		System.out.println("count =="+count);
+	}
+}
